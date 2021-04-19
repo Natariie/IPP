5c0e1b4fda5ddae291dd0965353c48d5d4360a7a
Arvid <arfr1901@student.miun.se>
2021-04-19 18:19:19 +0200

    (more or less) finished the timer function

--- app/src/main/java/com/example/projectipp/Activity2.kt
+++ app/src/main/java/com/example/projectipp/Activity2.kt
@@ -1,66 +1,174 @@
 package com.example.projectipp
 
+import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.projectipp.databinding.Activity2Binding
 
-class Activity2 : AppCompatActivity() {
+open class Activity2 : AppCompatActivity() {
+
+    companion object{
+        var isNewVal1 : Boolean = false
+        var isNewVal2 : Boolean = false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        val binding : Activity2Binding = Activity2Binding.inflate(layoutInflater)
-
         super.onCreate(savedInstanceState)
 
+        val binding : Activity2Binding = Activity2Binding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val popupMenu1 = PopupMenu(
-                this,
-                binding.setTimerValue1
+            this,
+            binding.setTimerValue1
         )
-
         val popupMenu2 = PopupMenu(
-                this,
-                binding.setTimerValue2
+            this,
+            binding.setTimerValue2
         )
 
         popupMenu1.menuInflater.inflate(R.menu.timer_menu1, popupMenu1.menu)
         popupMenu2.menuInflater.inflate(R.menu.timer_menu2, popupMenu2.menu)
 
-        val preferences = Preferences(this)
-        var timerPref1 =  preferences.getTimerValues1()
-
         popupMenu1.setOnMenuItemClickListener { menuItem ->
+
             val id = menuItem.itemId
 
-            when(id){
-                R.id.one -> binding.setTimerValue1.text = "1"
-                R.id.two -> binding.setTimerValue1.text = timerPref1.toString()
-                R.id.three -> binding.setTimerValue1.text = "3"
-                R.id.four -> binding.setTimerValue1.text = "4"
-            }
+            val prefVal1 = "1"
+            val prefVal2 = "2"
+            val prefVal3 = "3"
+            val prefVal4 = "4"
 
+            val sharedPreferences1: SharedPreferences = getSharedPreferences(
+                "sharedPrefs",
+                MODE_PRIVATE
+            )
+            val editor = sharedPreferences1.edit()
+
+            when(id){
+                R.id.one -> {
+                    binding.setTimerValue1.text = prefVal1
+                    editor.apply {
+                        putString("VAL1_KEY", prefVal1)
+                    }.apply()
+                    isNewVal1 = true
+                }
+                R.id.two -> {
+                    binding.setTimerValue1.text = prefVal2
+                    editor.apply {
+                        putString("VAL1_KEY", prefVal2)
+                    }.apply()
+                    isNewVal1 = true
+                }
+                R.id.three -> {
+                    binding.setTimerValue1.text = prefVal3
+                    editor.apply {
+                        putString("VAL1_KEY", prefVal3)
+                    }.apply()
+                    isNewVal1 = true
+                }
+                R.id.four -> {
+                    binding.setTimerValue1.text = prefVal4
+                    editor.apply {
+                        putString("VAL1_KEY", prefVal4)
+                    }.apply()
+                    isNewVal1 = true
+                }
+            }
 
             false
         }
 
-        preferences.setTimerValues1(timerPref1)
-        binding.setTimerValue1.text = timerPref1.toString()
+
+        val sharedPreferences1: SharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE)
+        val savedVal1 = sharedPreferences1.getString("VAL1_KEY", "1")
+        binding.setTimerValue1.text = savedVal1
+
 
         popupMenu2.setOnMenuItemClickListener { menuItem ->
+
             val id = menuItem.itemId
 
+            val prefVal1 = "seconds"
+            val prefVal2 = "minutes"
+            val prefVal3 = "hours"
+            val prefVal4 = "days"
+            val prefVal5 = "weeks"
+
+            val sharedPreferences: SharedPreferences = getSharedPreferences(
+                "sharedPrefs",
+                MODE_PRIVATE
+            )
+            val editor = sharedPreferences.edit()
+
             when(id){
-                R.id.minutes -> binding.setTimerValue2.text = R.string.minutes.toString()
-                R.id.two -> binding.setTimerValue2.text = R.string.hours.toString()
-                R.id.three -> binding.setTimerValue2.text = R.string.days.toString()
-                R.id.four -> binding.setTimerValue2.text = R.string.weeks.toString()
+                R.id.seconds -> {
+                    binding.setTimerValue2.text = prefVal1
+                    editor.apply {
+                        putString("VAL2_KEY", prefVal1)
+                    }.apply()
+                    isNewVal2 = true
+                }
+                R.id.minutes -> {
+                    binding.setTimerValue2.text = prefVal2
+                    editor.apply {
+                        putString("VAL2_KEY", prefVal2)
+                    }.apply()
+                    isNewVal2 = true
+                }
+                R.id.hours -> {
+                    binding.setTimerValue2.text = prefVal3
+                    editor.apply {
+                        putString("VAL2_KEY", prefVal3)
+                    }.apply()
+                    isNewVal2 = true
+                }
+                R.id.days -> {
+                    binding.setTimerValue2.text = prefVal4
+                    editor.apply {
+                        putString("VAL2_KEY", prefVal4)
+                    }.apply()
+                    isNewVal2 = true
+                }
+                R.id.weeks -> {
+                    binding.setTimerValue2.text = prefVal5
+                    editor.apply {
+                        putString("VAL2_KEY", prefVal5)
+                    }.apply()
+                    isNewVal2 = true
+
+                }
             }
 
-
             false
+        }
+
+
+        val sharedPreferences2: SharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE)
+        val savedVal2 = sharedPreferences2.getString("VAL2_KEY", "days")
+        binding.setTimerValue2.text = savedVal2
+
+        binding.saveButton.setOnClickListener(){
+
+            val sharedPreferences3: SharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE)
+            val savedVal1 = sharedPreferences3.getString("VAL1_KEY", "1")
+            binding.setTimerValue1.text = savedVal1
+
+            val sharedPreferences4: SharedPreferences = getSharedPreferences("sharedPrefs", MODE_PRIVATE)
+            val savedVal2 = sharedPreferences4.getString("VAL2_KEY", "days")
+            binding.setTimerValue2.text = savedVal2
+
+            val intent2 = Intent(this, MainActivity::class.java)
+            intent2.putExtra("VAL1_KEY", savedVal1)
+            intent2.putExtra("VAL2_KEY", savedVal2)
+
+            Toast.makeText(this, "Will send a reminder in: " + savedVal1 + " " + savedVal2, Toast.LENGTH_SHORT).show()
+
+            startActivity(intent2)
         }
 
         binding.setTimerValue1.setOnClickListener(){
@@ -70,6 +178,9 @@ class Activity2 : AppCompatActivity() {
         binding.setTimerValue2.setOnClickListener(){
             popupMenu2.show()
         }
+
+        isNewVal1 = false
+        isNewVal2 = false
 
     }
 
--- app/src/main/java/com/example/projectipp/MainActivity.kt
+++ app/src/main/java/com/example/projectipp/MainActivity.kt
@@ -1,23 +1,116 @@
 package com.example.projectipp
 
+import android.app.AlertDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectipp.databinding.Activity2Binding
 import com.example.projectipp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+open class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        super.onCreate(savedInstanceState)
+
         val binding : ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
 
-        super.onCreate(savedInstanceState)
+        val binding2 : Activity2Binding = Activity2Binding.inflate(LayoutInflater.from(this))
 
         setContentView(binding.root)
 
         binding.TimerButton.setOnClickListener(){
+
             val intent = Intent(this, Activity2::class.java)
             startActivity(intent)
+
+        }
+
+        val key1 = intent.getStringExtra("VAL1_KEY")
+        val key2 = intent.getStringExtra("VAL2_KEY")
+
+        if(key1 != null && key2 != null){
+            startTimer(key1, key2)
+        }
+        else if(key1 == null && key2 == null){
+            startTimer(
+                binding2.setTimerValue1.text as String?,
+                binding2.setTimerValue2.text as String?
+            )
+        }
+
+    }
+
+    open fun startTimer(key1: String?, key2: String?){
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Time for a change!")
+        builder.setMessage("This is a reminder that you need to replace your insulin pump.")
+
+        when(key1){
+            "1" -> {
+                if (key2 == "seconds") {
+                    val timer = object : CountDownTimer(1000, 1000) {
+                        override fun onTick(millisUntilFinished: Long) {}
+
+                        override fun onFinish() {
+                            builder.setPositiveButton("Got it!") { _, _ ->
+
+                            }
+                            builder.show()
+                        }
+                    }
+                    timer.start()
+                }
+            }
+            "2" -> {
+                if (key2 == "seconds") {
+                    val timer = object : CountDownTimer(2000, 1000) {
+                        override fun onTick(millisUntilFinished: Long) {}
+
+                        override fun onFinish() {
+                            builder.setPositiveButton("Got it!") { _, _ ->
+
+                            }
+                            builder.show()
+                        }
+                    }
+                    timer.start()
+                }
+            }
+            "3" -> {
+                if (key2 == "seconds") {
+                    val timer = object : CountDownTimer(3000, 1000) {
+                        override fun onTick(millisUntilFinished: Long) {}
+
+                        override fun onFinish() {
+                            builder.setPositiveButton("Got it!") { _, _ ->
+
+                            }
+                            builder.show()
+                        }
+                    }
+                    timer.start()
+                }
+            }
+            "4" -> {
+                if (key2 == "seconds") {
+                    val timer = object : CountDownTimer(4000, 1000) {
+                        override fun onTick(millisUntilFinished: Long) {}
+
+                        override fun onFinish() {
+                            builder.setPositiveButton("Got it!") { _, _ ->
+
+                            }
+                            builder.show()
+                        }
+                    }
+                    timer.start()
+                }
+            }
         }
 
     }
