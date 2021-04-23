76d156cfca932501f75d1ec01fe700ae23a02d12
natariie <natariie.apps@gmail.com>
2021-04-23 12:34:46 +0200

    Changed switching from activity to switching to fragments

--- project/app/src/main/java/com/example/ipp/MainActivity.kt
+++ project/app/src/main/java/com/example/ipp/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.ipp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var buttonStart : Button
@@ -12,12 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       buttonStart = findViewById(R.id.button_start)
-
-        buttonStart.setOnClickListener(View.OnClickListener {
-            Intent(this, ChoiceMenu::class.java).also {
-                startActivity(it)
-            }
-        })
+        // Used YT video as helping source for switching fragment https://youtu.be/Nj4H4Xg4rjE
+        buttonStart = findViewById(R.id.button_start)
+        buttonStart.setOnClickListener {
+            val start = FragmentChoiceMenu()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_start_menu, start)
+            // The addToBacStack should make the user able to go back to the last fragment by pressing the back button.
+            // However this is not needed for the main screen and therefore should be set to null
+            // What's odd is that this doesn't seem to apply in the emulator.
+            transaction.addToBackStack(null).commit()
+        }
     }
 }
+
+// Classic Bluetooh (not Low Energy Bluetooth)
+// Bluetooth Android documentation on https://developer.android.com/guide/topics/connectivity/bluetooth
+
+// Bluetooth YT video https://youtu.be/PtN6UTIu7yw
+
